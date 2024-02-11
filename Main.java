aba077ff26c1df18beac433e12b225919a73ea87
pugnaguido <144164356+pugnaguido@users.noreply.github.com>
2024-02-11 15:31:13 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//create teh main class as its always needed
+public class Main{
+    //create a main methodthat will be the entry point of our program
+    public static void main(string[] args){
+        //print out a title
+        System.out.println("\n\n\t** welcome, we ar egoing to be making swords**\n\n");
+
+        //create a good bye mnessage
+        System.out.println("\n\n\t*** thank you for making some swords with me**\n\n");
+        
+    }
+
+
+}
---
+++ sword.java
@@ -0,0 +1,24 @@
+// making a swordclass
+public class sword {
+    //properties 
+    private String material;
+    private String type;
+
+    //create constructor
+    public sword(String material, type){
+    this.material;
+    this.type;
+
+    }
+
+    //method 1
+    public void material(){
+        System.out.println("we need to pick out the material. this material will be"+ material +"");
+    }
+
+    //method 2 
+    public void create(){
+System.out.println("creating" + type + " using" + material + "Like a real blacksmith");
+    }
+
+} //end of sword class
